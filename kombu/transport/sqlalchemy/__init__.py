c3f62cd3ea02b8f9ae1061d73de5f79656953c69
Konstantin Matveev <matveevtam@yandex-team.ru>
2017-01-25 14:25:29 +0500

    Add pessimistic disconnect handling

--- kombu/transport/sqlalchemy/__init__.py
+++ kombu/transport/sqlalchemy/__init__.py
@@ -5,8 +5,8 @@
 from __future__ import absolute_import
 
 from anyjson import loads, dumps
-from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError
+from sqlalchemy import create_engine, select, event
+from sqlalchemy.exc import OperationalError, DBAPIError
 from sqlalchemy.orm import sessionmaker
 
 from kombu.five import Empty
@@ -20,6 +20,41 @@ from .models import (ModelBase, Queue as QueueBase, Message as MessageBase,
 
 VERSION = (1, 1, 0)
 __version__ = '.'.join(map(str, VERSION))
+
+
+def _ping_connection(connection, branch):
+    if branch:
+        # "branch" refers to a sub-connection of a connection,
+        # we don't want to bother pinging on these.
+        return
+
+    # turn off "close with result".  This flag is only used with
+    # "connectionless" execution, otherwise will be False in any case
+    save_should_close_with_result = connection.should_close_with_result
+    connection.should_close_with_result = False
+
+    try:
+        # run a SELECT 1.   use a core select() so that
+        # the SELECT of a scalar value without a table is
+        # appropriately formatted for the backend
+        connection.scalar(select([1]))
+    except DBAPIError as err:
+        # catch SQLAlchemy's DBAPIError, which is a wrapper
+        # for the DBAPI's exception.  It includes a .connection_invalidated
+        # attribute which specifies if this connection is a "disconnect"
+        # condition, which is based on inspection of the original exception
+        # by the dialect in use.
+        if err.connection_invalidated:
+            # run the same SELECT again - the connection will re-validate
+            # itself and establish a new connection.  The disconnect detection
+            # here also causes the whole connection pool to be invalidated
+            # so that all stale connections are discarded.
+            connection.scalar(select([1]))
+        else:
+            raise
+    finally:
+        # restore "close with result"
+        connection.should_close_with_result = save_should_close_with_result
 
 
 class Channel(virtual.Channel):
@@ -52,6 +87,7 @@ class Channel(virtual.Channel):
         conninfo = self.connection.client
         if conninfo.hostname not in self._engines:
             engine = self._engine_from_config()
+            event.listens_for(engine, "engine_connect")(_ping_connection)
             Session = sessionmaker(bind=engine)
             metadata.create_all(engine)
             self._engines[conninfo.hostname] = engine, Session
